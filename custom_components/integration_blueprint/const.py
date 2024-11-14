b7ce60f284f35ddf452f924553b6bc95358a3d17
juanillo62gm <juangomezm97@gmail.com>
2024-11-14 21:44:53 +0100

    feat(blueprint): start

---
+++ custom_components/integration_blueprint/const.py
@@ -0,0 +1,8 @@
+"""Constants for panda_pwr."""
+
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+DOMAIN = "panda_pwr"
+ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
